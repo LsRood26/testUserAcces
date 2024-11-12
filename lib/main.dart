5638e297d2667a098407e968a5e8d069aa3502c9
LsRood26 <carloschavezr26@gmail.com>
2024-11-12 08:26:38 -0500

    project firebase setup

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_useracces/firebase_options.dart';
+import 'package:test_useracces/providers/provider_login.dart';
+import 'package:test_useracces/screens/home.dart';
+import 'package:test_useracces/screens/login.dart';
+import 'package:test_useracces/screens/new_user.dart';
+import 'package:test_useracces/screens/register.dart';
+
+void main() async {
+  await WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Firebase.initializeApp();
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => ProviderLogin()),
+  ], child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        /* home: LoginPage(), */
+        routes: {
+          '/': (context) => LoginPage(),
+          '/register': (context) => RegisterPage(),
+          '/homepage': (context) => HomePage(),
+          '/newuser': (context) => NewUser(),
+        });
+  }
+}
